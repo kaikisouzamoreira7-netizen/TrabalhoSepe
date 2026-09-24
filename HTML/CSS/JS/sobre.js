05f0843b8093f7e320d79a4505d6b88757b105da
coelhoandreus-max <coelhoandreus@gmail.com>
2026-09-24 17:41:26 -0300

    Finalização da página sobre

---
+++ HTML/CSS/JS/sobre.js
@@ -0,0 +1,11 @@
+function guilherme()
+ { document.getElementById("informacao").innerHTML = "Guilherme Luciano Da Silva<br> Desenvolvedor da pagina comercio."; }
+
+function caua() 
+{ document.getElementById("informacao").innerHTML = "Cauã Wlodarczyk<br>Desenvolvedor da página Rotas e a poder."; }
+
+function kaiki()
+ { document.getElementById("informacao").innerHTML = "Kaiki De Souza Moreira<br>Desenvolvedor da página inicial."; }
+
+function andreus()
+ { document.getElementById("informacao").innerHTML = "Andreus Cesar Coelho<br>Desenvolvedor da página sobre."; }
